2587473534df62a715a0ed5b2a395f3b57894c2f
Mukul Raj <104729457+Mukulraj109@users.noreply.github.com>
2024-08-17 18:56:46 +0530

    Update user-repository.js

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,73 +1,90 @@
-const { User, Role } = require('../models/index');
-const ValidationError = require('../utils/validation-error');
+const {User,Role}= require("../models/index");
+const { StatusCodes }= require("http-status-codes");
+const ValidationError= require("../utils/errors/validation-error");
+const ClientError= require("../utils/errors/client-error");
 
-class UserRepository {
+class UserRepository{
 
-    async create(data) {
+    async create(data){
         try {
-            const user = await User.create(data);
+            const user= await User.create(data);
             return user;
         } catch (error) {
-            if(error.name == 'SequelizeValidationError') {
-                throw new ValidationError(error);
+            if(error.name==="SequelizeValidationError"){
+                const validationError= new ValidationError(error);
+                throw validationError;
             }
-            console.log("Something went wrong on repository layer");
+            console.log("Went Wrong in Repository Layer");
             throw error;
+            
         }
     }
 
-    async destroy(userId) {
+    async getById(userId){
         try {
-            await User.destroy({
-                where: {
-                    id: userId
-                }
-            });
-            return true;
-        } catch (error) {
-            console.log("Something went wrong on repository layer");
-            throw error;
-        }
-    }
+            
+            const user= await User.findByPk(userId, {
 
-    async getById(userId) {
-        try {
-            const user = await User.findByPk(userId, {
-                attributes: ['email', 'id']
+                attributes: ["email","id"] 
             });
             return user;
         } catch (error) {
-            console.log("Something went wrong on repository layer");
-            throw error;
+            console.log("Went Wrong in Repository Layer");
+            throw {error};
         }
     }
 
-    async getByEmail(userEmail) {
+    async getByEmail(email){
         try {
-            const user = await User.findOne({where: {
-                email: userEmail
-            }});
-            return user;
-        } catch (error) {
-            console.log("Something went wrong on repository layer");
-            throw error;
-        }
-    }
-
-    async isAdmin(userId) {
-        try {
-            const user = await User.findByPk(userId);
-            const adminRole = await Role.findOne({
-                where: {
-                    name: 'ADMIN'
+            const user= await User.findOne({
+                where:{
+                    email:email
                 }
             });
-            return user.hasRole(adminRole);
+
+            if(!user){
+                let errorName= "AttributeNotFound";
+                let errorMessages="Invalid Email Sent";
+                let errorExplaination= "Please check the Email, as there is no record of Email";
+                let statusCode= StatusCodes.NOT_FOUND;
+
+                const clientError= new ClientError(
+                    errorName,
+                    errorMessages,
+                    errorExplaination,
+                    statusCode
+                );
+
+                throw clientError;
+            }
+
+            return user;
         } catch (error) {
-            console.log("Something went wrong on repository layer");
+            if(error.name === "AttributeNotFound"){
+                throw error;
+            }
+            console.log("Went Wrong in Repository Layer");
             throw error;
+        }
+    }
+
+    async isAdmin(userId){
+        try {
+            const user= await User.findByPk(userId);
+            const adminRole= await Role.findOne({
+                where:{
+                    name:"ADMIN"
+                }
+            });
+
+            const result= await user.hasRole(adminRole);
+            return result;
+        } catch (error) {
+            
+            // console.log("Went Wrong in Repository Layer");
+            // throw {error};
         }
     }
 }
 
-module.exports = UserRepository;
+module.exports= UserRepository;
